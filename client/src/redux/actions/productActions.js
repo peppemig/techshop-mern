f132f3a60a3e60948ecc948edf7bac1896d893ec
Giuseppe <120139042+peppemig@users.noreply.github.com>
2023-04-02 17:17:31 +0200

    added filter options

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -35,4 +35,40 @@ export const getProduct = (id) => async (dispatch) => {
                 : "An unexpected error has occured. Please try again later."
             ))
     }
-} 
+}
+
+export const getProductsByBrand = (name) => async (dispatch) => {
+    dispatch(setLoading(true))
+
+    try {
+        const {data} = await axios.get(`http://localhost:5000/api/products/brand/${name}`)
+        dispatch(setProducts(data))
+    } catch (error) {
+        dispatch
+            (setError(
+                error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message 
+                ? error.message 
+                : "An unexpected error has occured. Please try again later."
+            ))
+    }
+}
+
+export const getProductsByPrice = (price) => async (dispatch) => {
+    dispatch(setLoading(true))
+
+    try {
+        const {data} = await axios.get(`http://localhost:5000/api/products/price/${price}`)
+        dispatch(setProducts(data))
+    } catch (error) {
+        dispatch
+            (setError(
+                error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message 
+                ? error.message 
+                : "An unexpected error has occured. Please try again later."
+            ))
+    }
+}
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,23 +1,52 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Center, Spinner, Stack, Wrap, WrapItem } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Center, Flex, Spinner, Stack, Wrap, WrapItem, Text, Button, Input, HStack, Select } from '@chakra-ui/react'
 import ProductCard from '../components/ProductCard'
 import { useDispatch, useSelector } from 'react-redux'
-import { getProducts } from '../redux/actions/productActions'
-import { useEffect } from 'react'
+import { getProducts, getProductsByBrand, getProductsByPrice } from '../redux/actions/productActions'
+import { useEffect, useState } from 'react'
 
 const ProductsScreen = () => {
+  const [priceForFilter, setPriceForFilter] = useState('')
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.products)
   const {loading, error, products} = productList;
+
+  const brands = ['Samsung', 'Apple', 'Sony']
 
   useEffect(() => {
     dispatch(getProducts())
   }, [dispatch])
 
   return (
+    <>
+
+    <Flex mt='10px' justifyContent='center' alignItems='center' flexDirection={{base: 'column', md: 'row'}} gap={{base: '20px', md: '30px'}}>
+
+      <Flex direction='column' gap='10px'>
+        <Text fontSize='lg' fontWeight='bold'>Filter by brand name:</Text>
+        <HStack>
+        <Select width='300px' onChange={(e) => e.target.value === '' ? dispatch(getProducts()) : dispatch(getProductsByBrand(e.target.value))}>
+                <option value=''>All</option>
+            {brands.map((brand) => (
+                <option value={brand}>{brand}</option>
+              ))
+            }
+        </Select>
+        </HStack>
+      </Flex>
+
+      <Flex direction='column' gap='10px'>
+        <Text fontSize='lg' fontWeight='bold'>Filter by max price:</Text>
+        <HStack>
+          <Input type='number' placeholder='Insert max price here' onChange={(e) => setPriceForFilter(e.target.value)}/>
+          <Button onClick={priceForFilter !== '' ? () => dispatch(getProductsByPrice(priceForFilter)) : () => dispatch(getProducts())}>Filter</Button>
+        </HStack>
+      </Flex>
+
+    </Flex>
     
     <Wrap spacing='30px' justify='center' minHeight='100vh'>
-
+        
         {loading ? (
           <Stack direction='row' spacing={4}>
             <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl'/>
@@ -29,7 +58,7 @@ const ProductsScreen = () => {
             <AlertTitle>Oops!</AlertTitle>
             <AlertDescription>{error}</AlertDescription>
           </Alert>
-        ) : (
+        ) : (  
 
           products.map((product) => (
               <WrapItem key={product._id} className='grow'>
@@ -38,9 +67,11 @@ const ProductsScreen = () => {
                   </Center>
               </WrapItem>
           ))
+
         )}
 
     </Wrap>
+    </>
   )
 }
 
